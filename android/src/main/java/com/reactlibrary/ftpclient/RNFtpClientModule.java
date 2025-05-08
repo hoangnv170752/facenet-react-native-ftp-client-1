792d331f958202c7543ed07bb72dd56524b22491
hoangf1 <hoangminato99@gmail.com>
2025-05-08 13:15:30 +0700

    update to build HCP version 1.0.4

--- android/src/main/java/com/reactlibrary/ftpclient/RNFtpClientModule.java
+++ android/src/main/java/com/reactlibrary/ftpclient/RNFtpClientModule.java
@@ -68,7 +68,13 @@ public class RNFtpClientModule extends ReactContextBaseJavaModule {
     this.reactContext = reactContext;
   }
 
-
+  /**
+   * Lưu các trường thông tin cần thiết trước khi login.
+   * @param ip_address
+   * @param port
+   * @param username
+   * @param password
+   */
   @ReactMethod
   public void setup(String ip_address, int port, String username, String password){
     this.ip_address = ip_address;
@@ -124,6 +130,11 @@ public class RNFtpClientModule extends ReactContextBaseJavaModule {
     return sdf.format(date);
   }
 
+  /**
+   * List ra các file và thư mục trong đường dẫn path được cung cấp.
+   * @param path
+   * @param promise
+   */
   @ReactMethod
   public void list(final String path, final Promise promise){
     new Thread(new Runnable() {
@@ -153,7 +164,12 @@ public class RNFtpClientModule extends ReactContextBaseJavaModule {
     }).start();
   }
 
-  //remove file or dir
+  /**
+   * Xoá đi file hoặc thư mục được chỉ định ở 'path', khi xoá sẽ phân biệt xoá
+   * file hay thư mục bằng cách nhận biết kí tự '/' ở cuối cùng nếu là thư mục.
+   * @param path
+   * @param promise
+   */
   @ReactMethod
   public void remove(final String path, final Promise promise){
     new Thread(new Runnable() {
@@ -163,7 +179,21 @@ public class RNFtpClientModule extends ReactContextBaseJavaModule {
         try {
           login(client);
           if(path.endsWith(File.separator)){
-            client.removeDirectory(path);
+            if(isEmptyDirectory(client, path)) {
+              boolean deleted = client.removeDirectory(path);
+              if (deleted) {
+                promise.resolve(true);
+              } else {
+                promise.reject("ERROR", "Failed to delete empty directory.");
+              }
+            } else {
+              boolean success = removeDirectoryRecursively(client, path);
+              if (success) {
+                promise.resolve(true);
+              } else {
+                promise.reject("ERROR", "Failed to delete non-empty directory.");
+              }
+            }
           }else{
             client.deleteFile(path);
           }
@@ -175,6 +205,31 @@ public class RNFtpClientModule extends ReactContextBaseJavaModule {
         }
       }
     }).start();
+  }
+
+  // Hàm đệ quy xóa thư mục không rỗng, vì phải xoá cả các file lẫn thu mục con.
+  private boolean removeDirectoryRecursively(FTPClient client, String remotePath) throws IOException {
+    FTPFile[] files = client.listFiles(remotePath);
+
+    for (FTPFile file : files) {
+      String fullPath = remotePath + "/" + file.getName();
+      if (file.isDirectory()) {
+        if (!removeDirectoryRecursively(client, fullPath)) {
+          return false;
+        }
+      } else {
+        if (!client.deleteFile(fullPath)) {
+          return false;
+        }
+      }
+    }
+    return client.removeDirectory(remotePath);
+  }
+
+  //check xem có phải thư mục rỗng không.
+  private boolean isEmptyDirectory(FTPClient client, String remotePath) throws IOException {
+    FTPFile[] files = client.listFiles(remotePath);
+    return files.length == 0;
   }
 
   private String makeToken(final String path,final String remoteDestinationDir ){
@@ -209,6 +264,12 @@ public class RNFtpClientModule extends ReactContextBaseJavaModule {
     return constants;
   }
 
+  /**
+   * upload file từ 'path' local trên điện thoại lên FTP server theo 'remoteDestinationPath'.
+   * @param path: đường dẫn file local ở máy điện thoại
+   * @param remoteDestinationPath: đường dẫn file trên FTP server
+   * @param promise
+   */
   @ReactMethod
   public void uploadFile(final String path,final String remoteDestinationPath, final Promise promise){
     final String token = makeToken(path,remoteDestinationPath);
@@ -221,75 +282,113 @@ public class RNFtpClientModule extends ReactContextBaseJavaModule {
       return;
     }
     final Thread t =
-    new Thread(new Runnable() {
-      @Override
-      public void run() {
-        FTPClient client = new FTPClient();
-        try {
-          login(client);
-          client.setFileType(FTP.BINARY_FILE_TYPE);
+            new Thread(new Runnable() {
+              @Override
+              public void run() {
+                FTPClient client = new FTPClient();
+                try {
+                  login(client);
+                  client.setFileType(FTP.BINARY_FILE_TYPE);
+                  client.setConnectTimeout(10000);  // timeout khi connect
+                  client.setSoTimeout(10000);       // timeout khi đọc ghi stream
+                  client.setDataTimeout(10000);     // timeout khi chờ server response
 
+                  long finishBytes = 0;
 
-          long finishBytes = 0;
+                  String remoteFile = remoteDestinationPath;
 
-          String remoteFile = remoteDestinationPath;
+                  String remoteFileConvert = URLDecoder.decode(remoteFile, "UTF-8");
+                  String localPathFileConvert = URLDecoder.decode(path, "UTF-8");
+                  File localFile = new File(localPathFileConvert);
+                  InputStream inputStream = new FileInputStream(localPathFileConvert);
+                  Log.d(TAG, "remoteFileConvert: " + remoteFileConvert + ", localPathFileConvert: " + localPathFileConvert);
+                  long totalBytes = localFile.length();
+                  Log.d(TAG,"Start uploading file: " + totalBytes);
 
-          String remoteFileConvert = URLDecoder.decode(remoteFile, "UTF-8");
-          String localPathFileConvert = URLDecoder.decode(path, "UTF-8");
-          File localFile = new File(localPathFileConvert);
-          InputStream inputStream = new FileInputStream(localPathFileConvert);
-          Log.d(TAG, "remoteFileConvert: " + remoteFileConvert + ", localPathFileConvert: " + localPathFileConvert);
-          long totalBytes = localFile.length();
-          Log.d(TAG,"Start uploading file: " + totalBytes);
+                  OutputStream outputStream = client.storeFileStream(remoteFileConvert);
+                  byte[] bytesIn = new byte[4096];
+                  int read = 0;
 
-          OutputStream outputStream = client.storeFileStream(remoteFileConvert);
-          byte[] bytesIn = new byte[4096];
-          int read = 0;
+                  sendProgressEventToToken(token,0);
+                  Log.d(TAG,"Resolve token:"+token);
+                  int lastPercentage = 0;
+                  while ((read = inputStream.read(bytesIn)) != -1 && !Thread.currentThread().isInterrupted()) {
+                    outputStream.write(bytesIn, 0, read);
+                    finishBytes += read;
+                    int newPercentage = (int)(finishBytes*100/totalBytes);
+                    if(newPercentage>lastPercentage){
+                      sendProgressEventToToken(token,newPercentage);
+                      lastPercentage = newPercentage;
+                    }
+                  }
+                  inputStream.close();
+                  outputStream.close();
+                  Log.d(TAG,"Finish uploading");
 
-          sendProgressEventToToken(token,0);
-          Log.d(TAG,"Resolve token:"+token);
-          int lastPercentage = 0;
-          while ((read = inputStream.read(bytesIn)) != -1 && !Thread.currentThread().isInterrupted()) {
-              outputStream.write(bytesIn, 0, read);
-              finishBytes += read;
-              int newPercentage = (int)(finishBytes*100/totalBytes);
-              if(newPercentage>lastPercentage){
-                sendProgressEventToToken(token,newPercentage);
-                lastPercentage = newPercentage;
+                  //if not interrupted
+                  if(!Thread.currentThread().isInterrupted()) {
+                    boolean done = client.completePendingCommand();
+
+                    if (done) {
+                      promise.resolve(true);
+                    } else {
+                      promise.reject(RNFTPCLIENT_ERROR_CODE_UPLOAD, localFile.getName() + " is not uploaded successfully.");
+                      client.deleteFile(remoteFile);
+                    }
+                  }else{
+                    //interupted, the file will deleted by cancel update operation
+                    promise.reject(RNFTPCLIENT_ERROR_CODE_UPLOAD,ERROR_MESSAGE_CANCELLED);
+                  }
+                } catch (Exception e) {
+                  promise.reject(RNFTPCLIENT_ERROR_CODE_UPLOAD,e.getMessage());
+                } finally {
+                  uploadingTasks.remove(token);
+                  logout(client);
+                }
               }
-          }
-          inputStream.close();
-          outputStream.close();
-          Log.d(TAG,"Finish uploading");
-
-          //if not interrupted
-          if(!Thread.currentThread().isInterrupted()) {
-            boolean done = client.completePendingCommand();
-
-            if (done) {
-              promise.resolve(true);
-            } else {
-              promise.reject(RNFTPCLIENT_ERROR_CODE_UPLOAD, localFile.getName() + " is not uploaded successfully.");
-              client.deleteFile(remoteFile);
-            }
-          }else{
-            //interupted, the file will deleted by cancel update operation
-            promise.reject(RNFTPCLIENT_ERROR_CODE_UPLOAD,ERROR_MESSAGE_CANCELLED);
-          }
-        } catch (IOException e) {
-          promise.reject(RNFTPCLIENT_ERROR_CODE_UPLOAD,e.getMessage());
-        } finally {
-          uploadingTasks.remove(token);
-          logout(client);
-        }
-      }
-    });
+            });
     t.start();
     uploadingTasks.put(token,t);
   }
 
+  /**
+   * Kiểm tra file có tên 'remoteFileName' đã tồn tại trên FTP server chưa.
+   * @param remoteDirectory: đường dẫn file trên FTP server.
+   * @param remoteFileName: tên file.
+   * @param promise
+   */
   @ReactMethod
-  public void makedir(final String path, final Promise promise){
+  public void checkFileExists(String remoteDirectory, String remoteFileName, Promise promise) {
+    new Thread(() -> {
+      Log.d(TAG, "checkFileExists: ");
+      FTPClient client = new FTPClient();
+      try {
+        login(client);
+        FTPFile[] files = client.listFiles(remoteDirectory);
+
+        boolean fileExists = false;
+        for (FTPFile file : files) {
+          if (file.getName().equals(remoteFileName)) {
+            fileExists = true;
+            break;
+          }
+        }
+        promise.resolve(fileExists);
+      } catch (IOException e) {
+        promise.reject("FTP_ERROR", e.getMessage());
+      } finally {
+        logout(client);
+      }
+    }).start();
+  }
+
+  /**
+   * Tạo thư mục mới theo đường dẫn 'path'.
+   * @param path
+   * @param promise
+   */
+  @ReactMethod
+  public void makeDir(final String path, final Promise promise){
     new Thread(new Runnable() {
       @Override
       public void run() {
@@ -305,6 +404,11 @@ public class RNFtpClientModule extends ReactContextBaseJavaModule {
     }).start();
   }
 
+  /**
+   * Huỷ upload file khi file đang trong quá trình upload từ điện thoại lên server.
+   * @param token
+   * @param promise
+   */
   @ReactMethod
   public void cancelUploadFile(final String token, final Promise promise){
 
@@ -348,6 +452,12 @@ public class RNFtpClientModule extends ReactContextBaseJavaModule {
     return Long.parseLong(response[1]);
   };
 
+  /**
+   * Download file từ 'remoteDestinationPath' trên server về 'path' trên điện thoại.
+   * @param path: đường dẫn trên điện thoại, nơi lưu file về.
+   * @param remoteDestinationPath: đường dẫn file trên FTP server.
+   * @param promise
+   */
   @ReactMethod
   public void downloadFile(final String path,final String remoteDestinationPath, final Promise promise){
     final String token = makeDownloadToken(path,remoteDestinationPath);
@@ -436,6 +546,11 @@ public class RNFtpClientModule extends ReactContextBaseJavaModule {
     downloadingTasks.put(token,t);
   }
 
+  /**
+   * Huỷ download file khi file đang được download từ server về điện thoại.
+   * @param token
+   * @param promise
+   */
   @ReactMethod
   public void cancelDownloadFile(final String token, final Promise promise){
 
@@ -456,6 +571,12 @@ public class RNFtpClientModule extends ReactContextBaseJavaModule {
     promise.resolve(true);
   }
 
+  /**
+   * Di chuyển File hoặc Folder sang đường dẫn mới (sang folder cha khác).
+   * @param sourcePath: đường dẫn file hiện tại trên FTP server.
+   * @param destinationPath: đường dẫn file mới trên FTP server.
+   * @param promise
+   */
   @ReactMethod
   public void moveFileOrDirectory(final String sourcePath, final String destinationPath, final Promise promise) {
     new Thread(new Runnable() {
@@ -476,6 +597,47 @@ public class RNFtpClientModule extends ReactContextBaseJavaModule {
           promise.reject("ERROR", e.getMessage());
         } finally {
           logout(client); // Đăng xuất
+        }
+      }
+    }).start();
+  }
+
+  private long calculateFolderSize(FTPClient client, String remotePath) throws IOException {
+    long totalSize = 0;
+
+    FTPFile[] files = client.listFiles(remotePath);
+
+    for (FTPFile file : files) {
+      String filePath = remotePath + "/" + file.getName();
+      if (file.isFile()) {
+        totalSize += file.getSize();
+      } else if (file.isDirectory() && !file.getName().equals(".") && !file.getName().equals("..")) {
+        totalSize += calculateFolderSize(client, filePath); // đệ quy
+      }
+    }
+
+    return totalSize;
+  }
+
+  /**
+   * Lấy dung lượng của ổ cứng NAS.
+   * @param remotePath
+   * @param promise
+   */
+  @ReactMethod
+  public void getFolderSize(String remotePath, Promise promise) {
+    new Thread(new Runnable() {
+      @Override
+      public void run() {
+        FTPClient client = new FTPClient();
+        try {
+          login(client);
+          long size = calculateFolderSize(client, remotePath);
+          promise.resolve((double)size); // trả về byte
+        } catch (IOException e) {
+          promise.reject("ERROR", e.getMessage());
+        } finally {
+          logout(client);
         }
       }
     }).start();
